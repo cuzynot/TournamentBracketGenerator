e1bce5f9089e971e45094aaa3a56df40f79fda5a
ma521 <yilistuffs510@gmail.com>
2018-09-18 14:00:43 -0400

    included round and match numbers

--- src/SingleBracket.java
+++ src/SingleBracket.java
@@ -6,19 +6,21 @@ public class SingleBracket extends Bracket{
 		Slot leftSlot, rightSlot;
 		int round, matchNumber;
 
-		Slot(ArrayList<Team> team1, ArrayList<Team> team2) {
+		Slot(ArrayList<Team> team1, ArrayList<Team> team2, int round, int matchNumber) {
 			this.teams1 = team1;
 			this.teams2 = team2;
+			this.round = round;
+			this.matchNumber = matchNumber;
 		}
 	}
 
 	Slot lastSlot;
 
 	SingleBracket(ArrayList<Team> teams) {
-		lastSlot = constructSlots(teams);
+		lastSlot = constructSlots(teams, 1, 1);
 	}
 
-	private Slot constructSlots(ArrayList<Team> teams) {
+	private Slot constructSlots(ArrayList<Team> teams, int round, int matchNumber) {
 		Slot s;
 
 		int mid = teams.size() / 2;
@@ -26,13 +28,13 @@ public class SingleBracket extends Bracket{
 		ArrayList<Team> teams1 = truncate(teams, 0, mid);
 		ArrayList<Team> teams2 = truncate(teams, mid, teams.size());
 
-		s = new Slot(teams1, teams2);
+		s = new Slot(teams1, teams2, round, matchNumber);
 
 		if (teams1.size() > 1) {
-			s.leftSlot = constructSlots(teams1);
+			s.leftSlot = constructSlots(teams1, round + 1, matchNumber * 2 - 1);
 		}
 		if (teams2.size() > 1) {
-			s.rightSlot = constructSlots(teams2);
+			s.rightSlot = constructSlots(teams2, round + 1, matchNumber * 2);
 		}
 
 		return s;
