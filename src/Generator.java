47a253513521557ce3e36614b342183e83da8d23
bl21812 <39709270+bl21812@users.noreply.github.com>
2018-10-08 12:23:59 -0400

    Update Generator.java

--- src/Generator.java
+++ src/Generator.java
@@ -2,17 +2,17 @@
  * [Generator.java]
  * Abstract generator object that outlines necessesities for single and double elimination bracket generators
  * Authors: Yili Liu and Brian Li
- * September 21, 2018
+ * October 09, 2018
  */
 
 public abstract class Generator {
 
-	Bracket bracket; //Holds (access to) the bracket object made
+	private Bracket bracket; //Holds (access to) the bracket object made
 
 	/**
 	 * getBracket
 	 * This method returns the Bracket generated
-	 * @return The generated Bracket object
+	 * @return bracket, The generated Bracket object
 	 */
 	public Bracket getBracket() {
 		return bracket;
