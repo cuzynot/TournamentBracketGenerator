05a365fae9ab7e4a2c01b5633d33f1c0312f7336
ma521 <yilistuffs510@gmail.com>
2018-10-10 18:10:12 -0400

    runnable launcher

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,21 +1,9 @@
 import java.awt.AWTException;
-import java.awt.Robot;
-import java.awt.event.InputEvent;
 
 public class Launcher {
 
 	public static void main(String[] args) throws AWTException, InterruptedException {
-//		new ManagementSystem();
-		
-		Robot r = new Robot();
-		
-		Thread.sleep(2000);
-		
-		for (int i = 0; i < 10000; i++) {
-			// Thread.sleep(5);
-			r.mousePress(InputEvent.BUTTON1_DOWN_MASK);
-			r.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
-		}
+		new ManagementSystem();
 	}
 
 }
